7b1425ddc2399245b05881583a558cd7cdc95d50
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-05-25 12:36:24 -0300

    Primer commit, creacion html y css

---
+++ src/assets/js/index.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+
+//Variables botones
+const btnCifrar = document.getElementById("btn-cifrar");
+const btnDecifrar = document.getElementById("btn-decifrar");
+
+//Evento click
+btnCifrar.addEventListener('Click', function())
+
+
+}
